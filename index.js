a22e9350c2285f3c184c5cf8ec2423a434afee59
Rachid Abu-Hassan <rachid@platform78.com>
2020-10-24 13:45:15 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // add solution here
+// theBeatlesPlay
+function theBeatlesPlay(musicians, instruments) {
+  var phrases = []
+  for (let i = 0; i < musicians.length; i++) {
+    phrases.push(`${musicians[i]} plays ${instruments[i]}`)
+  }
+  return phrases
+}
+
+
+// johnLennonFacts
+function johnLennonFacts(facts) {
+
+
+}
+
+
+// iLoveTheBeatles
+function iLoveTheBeatles(number) {
+
+
+}
